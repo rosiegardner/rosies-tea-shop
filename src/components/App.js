1877647c7889e5f08d4ed9cf2b754ed8e745c51a
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-11 11:45:36 -0700

    import header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Header from './Hearder';
 
 function App(){
   const tea = 'Rose hip & lavender'
   return (
     <React.Fragment>
+      <Header />
       <h1>Rosies Tea Shop</h1>
       <h4>{tea}</h4>
+      <hr />
     </React.Fragment>
   )
 }
