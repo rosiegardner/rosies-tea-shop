2700b81d0bdd3b21c64cc36183bdb548e64228fa
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-11 13:06:30 -0700

    add file with set up

---
+++ src/components/TeaControl.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import NewTeaForm from './NewTeaForm';
+import TeaList from './TeaList';
+
+class TeaControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default TeaControl;
