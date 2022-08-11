16502f9e9b64fd9dbde83540335188ac20c4320c
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-11 16:20:37 -0700

    update component to render selected tea

--- src/components/TeaDetail.js
+++ src/components/TeaDetail.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function TeaDetail(props){
+  const {tea} = props;
   return (
     <React.Fragment>
       <h1>Tea Information</h1>
+      <h3>{tea.name} - ${tea.price}.00 per .oz</h3>
+      <p><b>Type:</b> <em>{tea.blend}</em></p>
+      <p><b>Origin:</b> <em>{tea.origin}</em></p>
+      <h4>{tea.quantity} available</h4>
+      <h4>{tea.crate} crates in stock</h4>
       <hr />
     </React.Fragment>
   );
+}
+
+TeaDetail.propTypes = {
+  tea: PropTypes.object
 }
 
 export default TeaDetail;
