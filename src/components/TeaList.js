4e4ac085f99fc5b5abb42a2014adcd58c389e528
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-11 15:14:28 -0700

    pass new props to tl comp - add propTypes

--- src/components/TeaList.js
+++ src/components/TeaList.js
@@ -1,32 +1,14 @@
 import React from 'react';
 import Tea from './Tea';
+import PropTypes from 'prop-types';
 
-const mainTeaList = [
-  {
-    name: "Rosehip",
-    origin: "Europe, Asia and North Africa",
-    price: 2.99,
-    blend: "Herbal Tea",
-    quantity: 1,
-    crate: 130 
-  },
-  {
-    name: "Hojicha",
-    origin: "Kyoto",
-    price: 3.99,
-    blend: "Green Tea",
-    quantity: 1,
-    crate: 130
-  }
-]
-
-function TeaList(){
+function TeaList(props){
   return (
     <React.Fragment>
-      <hr/>
-      {mainTeaList.map((tea, index) => 
+      <hr />
+      {props.teaList.map((tea, index) => 
         <Tea 
-          name={tea.name}
+          names={tea.names}
           origin={tea.origin}
           price={tea.price}
           blend={tea.blend}
@@ -37,5 +19,9 @@ function TeaList(){
     </React.Fragment>
   );
 }
+
+TeaList.propTypes = {
+  teaList: PropTypes.array
+};
 
 export default TeaList;
