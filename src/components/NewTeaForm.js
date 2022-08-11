cab0d36abdfe9c8820666b77e08d8d6899b74f1c
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-11 13:42:15 -0700

    import uuid - add form

--- src/components/NewTeaForm.js
+++ src/components/NewTeaForm.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { v4 } from 'uuid';
 
 
-function NewTeaForm(props){
+function NewTeaForm(){
   return (
     <React.Fragment>
-      <h3>New Tea Form.</h3>
+      <form onSubmit={handleNewTeaFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Tea Name' 
+          required />
+        <input 
+          type='text'
+          name='blend'
+          placeholder='Type of Tea'
+          required />
+        <input
+          type='text'
+          name='origin'
+          placeholder='Origin of Tea'
+          required />
+        <input  
+          type='number'
+          name='price per oz'
+          placeholder='Price per ounce'
+          required />
+        <input 
+          type='number'
+          name='ounces per crate'
+          placeholder='Ounces per crate'
+          required />  
+        <input  
+          type='number'
+          name='stock quantity'
+          placeholder='Enter number of crates'
+          required />
+        <button type='submit'>Submit Tea</button>  
+      </form>
     </React.Fragment>
   );
 }
