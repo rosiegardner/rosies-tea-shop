158d3ff1895d7f800edd820df189972f56814b70
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-11 13:03:25 -0700

    add file for new tea form

---
+++ src/components/NewTeaForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function NewTeaForm(props){
+  return (
+    <React.Fragment>
+      <h3>New Tea Form.</h3>
+    </React.Fragment>
+  );
+}
+
+export default NewTeaForm;
