81907939622b3fe92c418fd8a9970408e3e5c26a
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-11 15:59:00 -0700

    add file with placeholder component

---
+++ src/components/TeaDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TeaDetail(props){
+  return (
+    <React.Fragment>
+      <h1>Tea Information</h1>
+      <hr />
+    </React.Fragment>
+  );
+}
+
+export default TeaDetail;
