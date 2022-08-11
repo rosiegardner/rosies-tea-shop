7bad490eb80dad23d2adcfd87697fa4e8de42cbe
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-11 12:17:05 -0700

    add props to pass to ticket component

--- src/components/TeaList.js
+++ src/components/TeaList.js
@@ -3,7 +3,13 @@ import Tea from './Tea';
 
 function TeaList(){
   return (
-    <Tea />
+    <Tea 
+      name="Rosehip"
+      origin="Europe, Asia and North Africa"
+      price="2.99 per oz"
+      blend="Herbal Tea"
+      quantity="1 oz"
+      crate="130 oz"/>
   );
 }
 
