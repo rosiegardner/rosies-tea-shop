40b4b0f5b4832a705ce01a23824e65ff31d81d3a
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-11 15:17:24 -0700

    utilize parent comp method in child comp

--- src/components/NewTeaForm.js
+++ src/components/NewTeaForm.js
@@ -1,45 +1,57 @@
 import React from 'react';
 import { v4 } from 'uuid';
+import PropTypes from 'prop-types';
 
+function NewTeaForm(props){
 
-function NewTeaForm(){
+  function handleNewTeaFormSubmission(event) {
+    event.preventDefault();
+    props.onNewTeaCreation({
+      name: event.target.name.value,
+      origin: event.target.origin.value,
+      price: event.target.price.value,
+      blend: event.target.blend.value,
+      quantity: event.target.quantity.value,
+      crate: event.target.crate.value,
+      id: v4()
+    });
+  }
+
   return (
     <React.Fragment>
       <form onSubmit={handleNewTeaFormSubmission}>
         <input
           type='text'
           name='name'
-          placeholder='Tea Name' 
-          required />
+          placeholder='Tea Name' />
         <input 
           type='text'
           name='blend'
-          placeholder='Type of Tea'
-          required />
+          placeholder='Type of Tea'/>
         <input
           type='text'
           name='origin'
-          placeholder='Origin of Tea'
-          required />
+          placeholder='Origin of Tea' />
         <input  
           type='number'
           name='price per oz'
-          placeholder='Price per ounce'
-          required />
+          placeholder='Price per ounce' />
         <input 
           type='number'
           name='ounces per crate'
-          placeholder='Ounces per crate'
-          required />  
+          placeholder='Ounces per crate'/>  
         <input  
           type='number'
           name='stock quantity'
-          placeholder='Enter number of crates'
-          required />
-        <button type='submit'>Submit Tea</button>  
+          placeholder='Enter number of crates' />
+        <button type='submit'>Add Tea to Stock</button>  
       </form>
     </React.Fragment>
   );
 }
+
+NewTeaForm.propTypes = {
+  onNewTeaCreation: PropTypes.func
+};
 
 export default NewTeaForm;
