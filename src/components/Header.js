0904c608511a43b8fd40062c8e46343ad992fc60
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-11 12:47:30 -0700

    add img to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import teaImage from './../img/tea.png';
 
 function Header(){
   return (
-    <h1>Rosies Tea House</h1>
+    <React.Fragment>
+      <h1>Rosies Tea House</h1>
+      <img src={teaImage} alt="Loose leaf earl grey"/>
+    </React.Fragment>
   );
 }
 
