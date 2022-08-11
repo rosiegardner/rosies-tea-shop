9b55ed976d36d6f9f4a8000da016c95673dd46aa
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-11 12:39:37 -0700

    add loop to render tea component

--- src/components/TeaList.js
+++ src/components/TeaList.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import Tea from './Tea';
 
+const mainTeaList = [
+  {
+    name: "Rosehip",
+    origin: "Europe, Asia and North Africa",
+    price: 2.99,
+    blend: "Herbal Tea",
+    quantity: 1,
+    crate: 130 
+  },
+  {
+    name: "Hojicha",
+    origin: "Kyoto",
+    price: 3.99,
+    blend: "Green Tea",
+    quantity: 1,
+    crate: 130
+  }
+]
+
 function TeaList(){
   return (
-    <Tea 
-      name="Rosehip"
-      origin="Europe, Asia and North Africa"
-      price="2.99 per oz"
-      blend="Herbal Tea"
-      quantity="1 oz"
-      crate="130 oz"/>
+    <React.Fragment>
+      <hr/>
+      {mainTeaList.map((tea, index) => 
+        <Tea 
+          name={tea.name}
+          origin={tea.origin}
+          price={tea.price}
+          blend={tea.blend}
+          quantity={tea.quantity}
+          crate={tea.crate}
+          key={index} />
+        )}
+    </React.Fragment>
   );
 }
 
