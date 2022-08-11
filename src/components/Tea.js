2878f5e7d420f83a0396e7f5b38f23571a07f878
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-11 11:49:59 -0700

    add file for tea component

---
+++ src/components/Tea.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Tea(){
+  const tea = 'Rose hip & lavender'
+  return (
+    <React.Fragment>
+      <h1>Rosies Tea Shop</h1>
+      <h4>{tea}</h4>
+    </React.Fragment>
+  );
+}
+
+export default Tea;
