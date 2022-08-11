09a53c9419209ed797908bb8401d3fd68bab5ae5
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-11 13:11:16 -0700

    add condition to component

--- src/components/TeaControl.js
+++ src/components/TeaControl.js
@@ -6,13 +6,21 @@ class TeaControl extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      formVisibleOnPage: false
+    };
   }
 
   render() {
+    let currentlyVisibleState = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTeaForm />
+    } else {
+      currentlyVisibleState = <TeaList />
+    }
     return (
       <React.Fragment>
-
+        {currentlyVisibleState}
       </React.Fragment>
     );
   }
