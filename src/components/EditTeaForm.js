e777b4b1eafe535024863992f0e7a72c9cf05000
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-12 12:27:31 -0700

    add event handler

--- src/components/EditTeaForm.js
+++ src/components/EditTeaForm.js
@@ -1,13 +1,34 @@
 import React from "react";
 import ReusableForm from './ReusableForm';
+import PropTypes from 'prop-types';
 
 function EditTeaForm (props) {
+  const {tea} = props;
+
+  function handleEditTeaFormSubmission(event) {
+    event.preventDefault();
+    props.onEditTea({
+      name: event.target.name.value,
+      origin: event.target.origin.value,
+      price: event.target.price.value,
+      blend: event.target.blend.value,
+      quantity: event.target.quantity.value,
+      crate: event.target.crate.value,
+      id: tea.id
+    });
+  }
   return (
     <React.Fragment>
       <ReusableForm
+        formSubmissionHandler={handleEditTeaFormSubmission}
         buttonText="Update Item" />
     </React.Fragment>
   );
 }
+
+EditTeaForm.propTypes = {
+  tea: PropTypes.object,
+  onEditTea: PropTypes.func
+};
 
 export default EditTeaForm;
