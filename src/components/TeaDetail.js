16d54e6ac58256a9b79bd23f208b18971b9e2135
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-14 00:16:49 -0700

    add onClick button for buying item

--- src/components/TeaDetail.js
+++ src/components/TeaDetail.js
@@ -2,16 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function TeaDetail(props){
-  const { tea, onClickingDelete } = props;
+  const { tea, onClickingDelete, onClickingBuy } = props;
   return (
     <React.Fragment>
       <h1>Tea Information</h1>
-      <h3>{tea.name} - ${tea.price} per .oz</h3>
+      <h3>{tea.name} - ${tea.price}.00 per .oz</h3>
       <p><b>Type:</b> <em>{tea.blend}</em></p>
       <p><b>Origin:</b> <em>{tea.origin}</em></p>
-      <p>{tea.quantity} available</p>
+      <p>{tea.quantity} ounces available</p>
       <p>{tea.crate} crates in stock</p>
+      <button onClick={() => onClickingBuy(tea.id) }>Buy Item</button>
+      <br></br>
+      <br></br>
       <button onClick={ props.onClickingEdit }>Update Item</button>
+      <br></br>
       <button onClick={() => onClickingDelete(tea.id) }>Delete Item</button>
       <hr />
     </React.Fragment>
@@ -21,7 +25,8 @@ function TeaDetail(props){
 TeaDetail.propTypes = {
   tea: PropTypes.object,
   onClickingDelete: PropTypes.func,
-  onClickingEdit: PropTypes.func
+  onClickingEdit: PropTypes.func,
+  onClickingBuy: PropTypes.func
 }
 
 export default TeaDetail;
